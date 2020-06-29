3d224654060d63d9342ebdc11ab6e3b190fae5e4
Patey0911 <mihalybogdanmatei@yahoo.com>
2020-06-29 14:48:37 +0300

    CC

---
+++ YOLO/YOLO.py
@@ -0,0 +1,9 @@
+import cv2
+import numpy as np
+
+net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
+classes = []
+with open("coco.names", "r") as f:
+    classes = [line.strip()for line in f.readlines()]
+
+print(classes)
